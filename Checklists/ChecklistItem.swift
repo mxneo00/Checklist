9998e0e6994e7f5bca3e313e2458c325e1012f17
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-21 23:43:30 -0500

    new file

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Katellyn Hyker on 1/21/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
