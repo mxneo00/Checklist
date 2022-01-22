b230493d15f26cdb0516c0c6ae467b9ce14e3223
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-21 23:06:11 -0500

    Table view delegates

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -12,7 +12,13 @@ class ChecklistViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    //MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 }
 
