743f9b1541feea49be5de70a21b65cd574dc96f8
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-22 10:25:23 -0500

    edits delegate protocol

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 protocol AddItemViewControllerDelegate: AnyObject {
     func addItemViewControllerDidCancel(_ controller: AddItemViewController)
     func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
+    func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
     
 }
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
@@ -34,10 +35,14 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
         delegate?.addItemViewControllerDidCancel(self)
     }
     @IBAction func done() {
-        let item = ChecklistItem()
-        item.text = textField.text!
-        
-        delegate?.addItemViewController(self, didFinishAdding: item)
+        if let item = itemToEdit {
+            item.text = textField.text!
+            delegate?.addItemViewController(self, didFinishEditing: item)
+        } else {
+            let item = ChecklistItem()
+            item.text = textField.text!
+            delegate?.addItemViewController(self, didFinishAdding: item)
+        }
     }
     //MARK: - Table View Delegates
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPAth: IndexPath) -> IndexPath? {
