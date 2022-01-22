c0e426487a94e132975355893316aca8f5a1f4bb
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-22 00:23:09 -0500

    View controller class

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Katellyn Hyker on 1/22/22.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
