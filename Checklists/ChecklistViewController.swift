eea95ef9f22ce2ada89c8356bdaa362daea44fe2
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-22 02:05:20 -0500

    new segue

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -71,10 +71,12 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
         return cell
     }
     func configureCheckmark( for cell: UITableViewCell, with  item: ChecklistItem) {
+        let label = cell.viewWithTag(1001) as! UILabel
+        
         if item.checked {
-            cell.accessoryType = .checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .none
+            label.text = ""
         }
     }
     func configureText( for cell: UITableViewCell, with item: ChecklistItem) {
