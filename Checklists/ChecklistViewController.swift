002067421e5cf5513ce1fd89091b5926e519e78e
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-21 23:02:47 -0500

    Prototype cell

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -11,7 +11,6 @@ class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
