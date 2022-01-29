583fb107dbb26d675b4714007d1426d0ff03aed3
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-29 17:47:39 -0500

    user defaults

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -37,6 +37,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UserDefaults.standard.set(indexPath.row, forKey:"ChecklistIndex")
         let checklist = dataModel.lists[indexPath.row]
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
