4df5366c9d973f04a4fe88977358158568d5e0de
hykerk <97618991+hykerk@users.noreply.github.com>
2022-01-29 13:10:01 -0500

    new load and save code

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -19,6 +19,8 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         
+        loadChecklists()
+        
         var  list = Checklist(name: "Birthdays")
         lists.append(list)
         
@@ -107,4 +109,32 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         }
         navigationController?.popViewController(animated: true)
     }
+    //MARK: - Data Saving
+    func documentsDirectory() -> URL {
+            let  paths = FileManager.default.urls(for:  .documentDirectory, in: .userDomainMask)
+            return paths[0]
+        }
+        func dataFilePath() -> URL {
+            return documentsDirectory().appendingPathComponent("Checklists.plist")
+        }
+        func saveChecklists() {
+            let encoder  = PropertyListEncoder()
+            do {
+                let data = try encoder.encode(lists)
+                try data.write(to: dataFilePath(),options: Data.WritingOptions.atomic)
+            } catch {
+                print("Error encoding item array: \(error.localizedDescription)")
+            }
+        }
+        func loadChecklists() {
+            let path = dataFilePath()
+            if let data = try? Data(contentsOf: path) {
+                let decoder = PropertyListDecoder()
+                do {
+                    lists  = try decoder.decode([Checklist].self, from: data)
+                } catch {
+                    print("Error decoding item array \(error.localizedDescription)")
+                }
+            }
+        }
 }
